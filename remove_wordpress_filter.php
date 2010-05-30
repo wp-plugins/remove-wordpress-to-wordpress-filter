5546d11976c5a6382c19f0c8dc8c130e3f21a679
tomthewebmaster <tomthewebmaster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-30 20:50:57 +0000

    small fix
    git-svn-id: https://plugins.svn.wordpress.org/remove-wordpress-to-wordpress-filter/tags/1.0@247377 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- remove_wordpress_filter.php
+++ remove_wordpress_filter.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Remove WordPress to Wordpress filter
+Plugin Name: Remove Wordpress to WordPress filter
 Plugin URI: http://tomlany.net/2010/05/wordpress-to-wordpress/
 Description: In WordPress 3.0, a filter changes all instances of "Wordpress" in posts, titles, and comments to "WordPress". This turns the filter off.
 Version: 1.0
