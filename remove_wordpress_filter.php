fc3184ea12904c3d74ccd81f3fe0b47a37193692
tomthewebmaster <tomthewebmaster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-09 04:30:45 +0000

    Version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/remove-wordpress-to-wordpress-filter/tags/1.3@801405 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ remove_wordpress_filter.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Remove Wordpress to WordPress filter
+Plugin URI: http://tomlany.net/2010/05/wordpress-to-wordpress/
+Description: This turns off the default filter that changes all instances of "Wordpress" to "WordPress".
+Version: 1.2
+Author: Tom Lany
+Author URI: http://tomlany.net/
+License: GPLv2 or later
+*/
+
+/* For more information about this plugin, see the readme.txt file included with the download package. */
+
+foreach ( array( 'the_content', 'the_title', 'wp_title', 'comment_text' ) as $filter ) {
+	$priority = has_filter( $filter, 'capital_P_dangit' );
+	if ( $priority !== FALSE ) {
+		remove_filter( $filter, 'capital_P_dangit', $priority );
+	}
+}
+
+?>
